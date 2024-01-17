5308f5005f895d2c4c104e0ba49757e1c56c6a8f
Kylie Clark <kyliedarlene@icloud.com>
2024-01-17 10:43:06 -0700

    added useEffect with fetch, passing plants [] to PlantList; cards not yet loading

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,15 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants }) {
+  console.log("plants [] in PlantList: " + plants);
+  
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map((plant) => {
+        <PlantCard key={plant.id} image={plant.image} name={plant.name} price={plant.price} />
+      })}
+    </ul>
   );
 }
 
